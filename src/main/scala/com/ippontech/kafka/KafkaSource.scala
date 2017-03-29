023842e41d1ecbd4746f0c55dd4d1a009e8fd70b
Luca Rosellini <lrosellini@keedio.com>
2017-03-29 14:35:36 +0200

    Better handling of latest readable offset

--- src/main/scala/com/ippontech/kafka/KafkaSource.scala
+++ src/main/scala/com/ippontech/kafka/KafkaSource.scala
@@ -4,6 +4,7 @@ import com.ippontech.kafka.stores.OffsetsStore
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import kafka.message.MessageAndMetadata
 import kafka.serializer.Decoder
+import org.apache.commons.lang.StringUtils
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.streaming.kafka.KafkaUtils
@@ -12,19 +13,30 @@ import scala.reflect.ClassTag
 
 object KafkaSource extends LazyLogging {
 
-  // Kafka input stream
   def kafkaStream[K: ClassTag, V: ClassTag, KD <: Decoder[K] : ClassTag, VD <: Decoder[V] : ClassTag]
   (ssc: StreamingContext, brokers: String, offsetsStore: OffsetsStore, topic: String): InputDStream[(K, V)] = {
-
+    kafkaStream[K,V,KD,VD](ssc, brokers, offsetsStore, topic, null)
+  }
+  
+  // Kafka input stream
+  def kafkaStream[K: ClassTag, V: ClassTag, KD <: Decoder[K] : ClassTag, VD <: Decoder[V] : ClassTag]
+  (ssc: StreamingContext, brokers: String, offsetsStore: OffsetsStore, topic: String, offsetReset: String): InputDStream[(K, V)] = {
+    
     val topics = Set(topic)
-    val kafkaParams = Map("metadata.broker.list" -> brokers)
-
     val storedOffsets = offsetsStore.readOffsets(topic)
     val kafkaStream = storedOffsets match {
       case None =>
         // start from the latest offsets
+        val kafkaParams = if (StringUtils.isNotEmpty(offsetReset)){
+          require(Array("smallest", "largest").contains(offsetReset))
+          Map("metadata.broker.list" -> brokers, "auto.offset.reset" -> offsetReset)
+        } else {
+          Map("metadata.broker.list" -> brokers, "auto.offset.reset" -> "smallest")
+        }
+        
         KafkaUtils.createDirectStream[K, V, KD, VD](ssc, kafkaParams, topics)
       case Some(fromOffsets) =>
+        val kafkaParams = Map("metadata.broker.list" -> brokers)
         // start from previously saved offsets
         val messageHandler = (mmd: MessageAndMetadata[K, V]) => (mmd.key, mmd.message)
         KafkaUtils.createDirectStream[K, V, KD, VD, (K, V)](ssc, kafkaParams, fromOffsets, messageHandler)
